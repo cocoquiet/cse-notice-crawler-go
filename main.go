c53aeba615d7b6eff5f6da42c77aed09071940fe
JiSeong Lee <cocoquiet@knu.ac.kr>
2025-01-28 20:14:59 +0900

    Feat: Add args

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"time"
 
 	"main.go/crawler"
@@ -9,10 +11,21 @@ import (
 
 func main() {
 	fmt.Println(time.Now().UTC(), "- Start crawling")
-	noticeList := crawler.CrawlNoticeFromWeb("전체", 50)
+	args := os.Args
+	if len(args) != 3 {
+		fmt.Printf("Usage: %s <notice amount> <url>\n", args[0])
+		return
+	}
+
+	amount, err := strconv.Atoi(args[1])
+	if err != nil {
+		fmt.Println("Invalid notice amount:", err)
+		return
+	}
+	noticeList := crawler.CrawlNoticeFromWeb("전체", amount)
 	fmt.Println(time.Now().UTC(), "- Finish crawling")
 
-	crawler.SendNoticeToAPI("https://uqywguq.request.dreamhack.games", noticeList)
+	crawler.SendNoticeToAPI(args[2], noticeList)
 
 	fmt.Println(time.Now().UTC(), "- Finish Sending")
 }
